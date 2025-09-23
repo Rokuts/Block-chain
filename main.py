2c3b499774bc7c3670dff4841f8d0a331047c9c7
Rokinis <147270094+Rokuts@users.noreply.github.com>
2025-09-24 00:08:27 +0300

    papildytas hash generatorius

--- main.py
+++ main.py
@@ -17,5 +17,15 @@ def hash_generator(tekstas):
     return hash
 
 if __name__ == "__main__":
-   tekstas = input("Įvesk tekstą: ")
-   print("Tavo hash:", hash_generator(tekstas))
+    if len(sys.argv) > 1:
+        failo_pavadinimas = sys.argv[1]
+        try:
+            with open(failo_pavadinimas, "r", encoding="utf-8") as f:   # atidaromo failo pavadinimas, rezimas (read), failo koduote
+                tekstas = f.read()
+        except FileNotFoundError:
+            print(f"Klaida: failas '{failo_pavadinimas}' nerastas.")
+            sys.exit(1)
+    else:
+        tekstas = input("Įvesk tekstą: ")
+
+    print("Tavo hash:", hash_generator(tekstas))
