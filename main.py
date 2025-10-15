7b3e07bc083fe583b2f1aea1df73bb60088eb938
Rokinis <147270094+Rokuts@users.noreply.github.com>
2025-10-16 02:07:18 +0300

    Pagerinta hash funkcija

--- main.py
+++ main.py
@@ -7,12 +7,16 @@ def hash_generator(tekstas):
     # Paverčiam tekstą į sąrašą skaičių (ASCII)
     skaiciai = [ord(c) for c in tekstas]   # skaiciu masyvas
 
-    # Atlik keletą matematikos operacijų
-    pirminis = 828930167
-    suma = pirminis  # pradinis sumos reikšmė
+    
+    d1 = 828930167
+    suma = d1  # pradinis sumos reikšmė
+
     for i, skaicius in enumerate(skaiciai):
-        suma = suma ^ (skaicius * pirminis)  # XOR su kiekvienu skaičiumi * pirminis
-        suma = (suma * pirminis) % 0x100000000  # daliname is Ox100000000, kad isvengtume per daug simboliu hash
+        suma = suma ^ (skaicius * d1)  # XOR su kiekvienu skaičiumi * d1
+        suma = (suma * d1) % 0x100000000  # daliname is Ox100000000, kad isvengtume per daug simboliu hash
+
+    # Papildomas maišymas pabaigoje
+    suma = (suma << 13 | suma >> (32 - 13)) & 0xFFFFFFFF
 
     hash = f"{suma:08x}" # skaiciaus formatavimas i hex su 8 simboliais
     return hash
